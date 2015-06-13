6d9ace46f25b8c432f3720dd419cf4b091594928
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-06-13 18:35:18 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ['Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-imagehelper',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/tk0miya/sphinxcontrib-imagehelper',
     license='BSD',
     author='Takeshi KOMIYA',
