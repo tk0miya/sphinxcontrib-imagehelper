503edbb23980251bff25636502380e39663f825d
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-10-07 23:46:55 +0900

    Remove unused utility functions

--- sphinxcontrib/imagehelper/utils.py
+++ sphinxcontrib/imagehelper/utils.py
@@ -17,24 +17,3 @@ def get_imagedir(app, docname):
 
     abspath = os.path.join(app.builder.outdir, dirname)
     return (relpath, abspath)
-
-
-def is_outdated(source, destination):
-    if not os.path.exists(source):
-        return False
-    else:
-        last_modified = os.stat(source).st_mtime
-        if not os.path.exists(destination) or os.stat(destination).st_mtime < last_modified:
-            return True
-        else:
-            return False
-
-
-def if_outdated(fn):
-    def wrap(self, filename, to, **kwargs):
-        if is_outdated(filename, to):
-            return fn(self, filename, to, **kwargs)
-        else:
-            return True
-
-    return wrap
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,8 +2,8 @@
 
 import os
 import sys
-from sphinx_testing import with_app, with_tmpdir
-from sphinxcontrib.imagehelper.utils import is_outdated, get_imagedir
+from sphinx_testing import with_app
+from sphinxcontrib.imagehelper.utils import get_imagedir
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -35,25 +35,3 @@ class TestSphinxcontrib(unittest.TestCase):
         (relpath, abspath) = get_imagedir(app, 'subdir/index')
         self.assertEqual('', relpath)
         self.assertEqual(os.path.join(app.outdir, ''), abspath)
-
-    @with_tmpdir
-    def test_is_outdated(self, dir):
-        source = dir / 'source.asta'
-        destination = dir / 'output.png'
-
-        # source file not exists
-        self.assertEqual(False, is_outdated(source, destination))
-
-        # destination file not exists
-        source.write_text('')
-        self.assertEqual(True, is_outdated(source, destination))
-
-        # destination file is newer than source
-        last_modified = os.stat(source).st_mtime
-        destination.write_text('')
-        destination.utime((last_modified + 1, last_modified + 1))
-        self.assertEqual(False, is_outdated(source, destination))
-
-        # destination file is older than source
-        destination.utime((last_modified - 1, last_modified - 1))
-        self.assertEqual(True, is_outdated(source, destination))
